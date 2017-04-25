fbd55757328d4c7a2cbeee5d2396b03036c70948
lena tuseeva <lenok-cotik@yandex.ru>
2017-04-25 16:01:52 -0700

    last update

--- my-traffic.py
+++ my-traffic.py
@@ -1,4 +1,5 @@
 import os
+import time
 import random
 import skimage.data
 import skimage.transform
@@ -12,62 +13,108 @@ def load_data(data_dir):
     directories = [d for d in os.listdir(data_dir) 
                    if os.path.isdir(os.path.join(data_dir, d))]
     labels = []
+    labels_str = {}
     images = []
+
+    i = 0
     for d in directories:
         label_dir = os.path.join(data_dir, d)
         file_names = [os.path.join(label_dir, f) 
-                      for f in os.listdir(label_dir) if f.endswith(".jpg")]
+                      for f in os.listdir(label_dir) if f.endswith(".ppm")]
+                      # for f in os.listdir(label_dir) if f.endswith(".jpg")]
+        labels_str[i] = d
         for f in file_names:
             images.append(skimage.data.imread(f))
-            labels.append(int(d))
-    return images, labels
+            labels.append(i)
+            
+        i = i + 1
+    return images, labels, labels_str
 
-def display_images_and_labels(images, labels):
-    unique_labels = set(labels)
-    plt.figure(figsize=(15, 15))
-    i = 1
-    for label in unique_labels:
-        image = images[labels.index(label)]
-        plt.subplot(8, 8, i)  # A grid of 8 rows x 8 columns
+
+def test_data(labels_str):
+    print('Select directory with testing data')
+    # test_data_dir = '/home/lena/Desktop/traffic-sign-recognition/images/testing'
+    test_data_dir = askdirectory()
+    print('Testing directory:', test_data_dir)
+
+    print('Loading testing images...')
+    start_time = time.time()
+    try:
+        test_images, test_labels, test_labels_str = load_data(test_data_dir)
+    except Error:
+        print('Testing data - error loading')
+    print('Loading data has ended: ', time.time() - start_time)
+
+    start_time = time.time()
+    print('Running resize images')
+    test_images32 = [skimage.transform.resize(image, (32, 32))
+                     for image in test_images]
+    predicted = session.run([predicted_labels], 
+                            feed_dict={images_ph: test_images32})[0]
+    print('Resizing images has ended...: ', time.time() - start_time)
+    
+    # for i in predicted:
+        # print(labels_str[i])
+
+    print('Check truthfulness graph...')
+    sum = 0
+    for i in range(len(predicted)):
+        if predicted[i] == test_labels[i]:
+            sum = sum + 1
+
+    print('Truthfulness: ', (sum / len(test_labels)) * 100, '%');
+
+
+def run_on_image(labels_str):
+    print('Select image')
+    image_path = askopenfilename()
+    print('Image path:', image_path)
+    
+    test_images = []    
+    test_images.append(skimage.data.imread(image_path))
+    
+    test_images32 = [skimage.transform.resize(image, (32, 32))
+                     for image in test_images]
+    predicted = session.run([predicted_labels], 
+                            feed_dict={images_ph: test_images32})[0]
+
+    for i in predicted:
+        print(labels_str[i])
+
+    for i in range(len(predicted)):
+        plt.subplot(1, 1, i + 1)
+        plt.text(0, -5, "It's - {0}".format(labels_str[predicted[i]]), 
+                     fontsize=12, color='black')
+        plt.imshow(test_images[i])
         plt.axis('off')
-        plt.title("Label: {0} ({1})".format(label, labels.count(label)))
-        i += 1
-        _ = plt.imshow(image)
     plt.show()
 
-# Dict
-str_labels = {
-    0: 'no parking',
-    1: 'no entry',
-    2: 'stop',
-}
 
-# print('Select folder with training images')
-# train_data_dir = askdirectory()
-# print('Training diractory:', train_data_dir)
+print('Select folder with training images')
+# train_data_dir = '/home/lena/Desktop/traffic-sign-recognition/images/training'
+train_data_dir = askdirectory()
+print('Training directory:', train_data_dir)
 
-# -------------
-# Todo: Select path with dialog window
-ROOT_PATH = "/home/lena/Desktop/course_work/images"
-train_data_dir = os.path.join(ROOT_PATH, "training")
-test_data_dir = os.path.join(ROOT_PATH, "testing")
+print('Loading training images...')
+start_time = time.time()
+try:
+    images, labels, labels_str = load_data(train_data_dir)
+except Error:
+    print('Training data - error loading')
+print('Loading data has ended: ', time.time() - start_time)
+print('Load ', len(images), 'images')
 
-# ROOT_PATH = "/home/lena/Desktop/course_work/traffic"
-# train_data_dir = os.path.join(ROOT_PATH, "datasets/BelgiumTS/Training")
-# test_data_dir = os.path.join(ROOT_PATH, "datasets/BelgiumTS/Testing")
-
-images, labels = load_data(train_data_dir)
 amount_images = len(set(labels))
 
-# -------------
-# Resize images
+start_time = time.time()
+print('Running resize images')
 images32 = [skimage.transform.resize(image, (32, 32)) for image in images]
+print('Resizing images has ended...: ', time.time() - start_time)
 
 images_a = np.array(images32)
 labels_a = np.array(labels)
 
-# -------------
-# Create graph
+print('Graph has created...')
 graph = tf.Graph()
 
 with graph.as_default():
@@ -79,72 +126,23 @@ with graph.as_default():
     predicted_labels = tf.argmax(logits, 1)
     loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels_ph, 
         logits=logits))
-    train = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(loss)
+    train = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
     init = tf.global_variables_initializer()
 
 session = tf.Session(graph=graph)
 _ = session.run([init])
 
+print('Session has created...')
+start_time = time.time()
+print('Running train...')
 for i in range(200):
     _, loss_value = session.run([train, loss], 
                                 feed_dict={images_ph: images_a, labels_ph: labels_a})
+print('Train has ended: ', time.time() - start_time)
 
-while (True): 
-    sample_indexes = random.sample(range(len(images32)), 1)
-    print('sample_indexes = ', sample_indexes)
+# Testing
+test_data(labels_str)
 
-    sample_images = [images32[i] for i in sample_indexes]
-    sample_labels = [labels[i] for i in sample_indexes]
-
-    predicted = session.run([predicted_labels], 
-                            feed_dict={images_ph: sample_images})[0]
-    print(sample_labels)
-    print(predicted)
-
-    predicted = session.run([predicted_labels], feed_dict={images_ph: sample_images})[0]
-    
-    fig = plt.figure(figsize=(10, 10))
-    for i in range(len(sample_images)):
-        print('i = ', i)
-        truth = sample_labels[i]
-        print('truth = ', truth)
-        prediction = predicted[i]
-        print('prediction = ', prediction)
-
-        plt.subplot(1, 2, i + 1)
-        plt.axis('off')
-
-        color='green' if truth == prediction else 'red'
-        plt.text(40, 10, 'Prediction: {0}'.format(str_labels[prediction]), 
-                 fontsize=12, color=color)
-
-        plt.imshow(sample_images[i])
-
-        # plt.subplot(1, 2, i + 2)
-        # plt.axis('off')
-        # plt.imshow(images32[sample_indexes[i]])
-
-    plt.show()
-
-    inp = input("Next?\n")
-    if inp != "yes":
-        break
-
-# ---------
-# Test data
-test_images, test_labels = load_data(test_data_dir)
-test_images32 = [skimage.transform.resize(image, (32, 32))
-                 for image in test_images]
-
-predicted = session.run([predicted_labels], feed_dict={images_ph: test_images32})[0]
-print(predicted)
-
-for i in range(len(predicted)):
-    plt.subplot(5, 2, i + 1)
-    plt.axis('off')
-    plt.text(40, 10, "Prediction: {0}".format(str_labels[predicted[i]]), 
-                 fontsize=12, color='black')
-    plt.imshow(test_images[i])
-plt.show()
+# run_on_image(labels_str)
 
 session.close()
