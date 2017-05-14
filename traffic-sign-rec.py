8a1d2e4562f9ebca92d98dcbf34ac502303939d6
lena tuseeva <lenok-cotik@yandex.ru>
2017-05-14 12:28:30 -0700

    ...

--- traffic-sign-rec.py
+++ traffic-sign-rec.py
@@ -6,6 +6,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
+from tkinter.filedialog import *
 
 def load_data(data_dir):
     directories = [d for d in os.listdir(data_dir)
@@ -19,7 +20,6 @@ def load_data(data_dir):
         label_dir = os.path.join(data_dir, d)
         file_names = [os.path.join(label_dir, f)
                       for f in os.listdir(label_dir)]
-        			# for f in os.listdir(label_dir) if f.endswith(".ppm")]
         labels_str[i] = d
         for f in file_names:
             images.append(skimage.data.imread(f))
@@ -28,16 +28,17 @@ def load_data(data_dir):
     return images, labels, labels_str
 
 def test_data(labels_str):
-    print('Введите директорию, в которой хранятся данные для тестирования')
+    print('Введите директорию, в которой хранятся данные для тестирования:')
     print('/home/lena/Desktop/traffic-sign-recognition/Знаки ПДД/Тестовые данные')
     test_data_dir = '/home/lena/Desktop/traffic-sign-recognition/Знаки ПДД/Тестовые данные'
-    # test_data_dir = input("директория: ")
+    test_data_dir = askdirectory()
     print('Выбранная директория:', test_data_dir)
 
     print('Загрузка тестовый изображений...')
     start_time = time.time()
     test_images, test_labels, test_labels_str = load_data(test_data_dir)
     print('Загрузка изображений была успешно завершена за ', time.time() - start_time, ' милисекунд')
+    print('Всего загружено ', len(test_images), ' изображений')
 
     start_time = time.time()
     print('Производится сжатие изображений до 32x32 пикселя...')
@@ -47,9 +48,6 @@ def test_data(labels_str):
                             feed_dict={images_ph: test_images32})[0]
     print('Сжатие изображений завершено за ', time.time() - start_time, ' милисекунд')
 
-    # for i in predicted:
-    # 	print(labels_str[i])
-
     print('Проверка правдивости распознавателя:')
     sum = 0
     for i in range(len(predicted)):
@@ -57,16 +55,12 @@ def test_data(labels_str):
             sum = sum + 1
 
     print('Правдивость равна = ', (sum / len(test_labels)) * 100, '%')
+    return
 
 def run_on_image(labels_str):
-    print('Введите путь изображение которое хотите распознать: ')
-    try:
-    	image_path = input("Путь до изображения: ")
-    except Exception as e:
-    	print('Error')
-    	return
-    # print('Путь до изображения:', image_path)
-
+    print('Выберите путь до изображения которое хотите распознать: ')
+    image_path = askopenfilename()
+    print(image_path)
     test_images = []
     test_images.append(skimage.data.imread(image_path))
 
@@ -74,32 +68,33 @@ def run_on_image(labels_str):
                      for image in test_images]
     predicted = session.run([predicted_labels],
                             feed_dict={images_ph: test_images32})[0]
-
-    # for i in predicted:
-    #     print(labels_str[i]) labels_str[predicted[i]]
-
     for i in range(len(predicted)):
         plt.subplot(1, 1, i + 1)
         print(labels_str[predicted[i]])
         plt.text(0, -5, labels_str[predicted[i]], fontsize=18, color='black')
         plt.imshow(test_images[i])
         plt.axis('off')
-    plt.show()
+    return
 
 mpl.rcParams['font.family'] = 'fantasy'
 mpl.rcParams['font.fantasy'] = 'Arial' # Для Windows
 mpl.rcParams['font.fantasy'] = 'Ubuntu' # Для Ubuntu
 mpl.rcParams['font.fantasy'] = 'Arial, Ubuntu'
 
-print('Введите директорию, в которой хранятся тренировочные изображения.')
-print('/home/lena/Desktop/traffic-sign-recognition/Знаки ПДД/Тренировочные данные')
-train_data_dir = '/home/lena/Desktop/traffic-sign-recognition/Знаки ПДД/Тренировочные данные'
-# train_data_dir = input("директория: ")
-print('Выбранная директория: ', train_data_dir)
+while True:
+    try:
+        print('Введите директорию, в которой хранятся тренировочные изображения:')
+        print('/home/lena/Desktop/traffic-sign-recognition/Знаки ПДД/Тренировочные данные')
+        train_data_dir = '/home/lena/Desktop/traffic-sign-recognition/Знаки ПДД/Тренировочные данные'
+        # train_data_dir = askdirectory()
+        print('Выбранная директория: ', train_data_dir)
+        print('Проводится загрузка тренировочных изображений...')
+        start_time = time.time()
+        images, labels, labels_str = load_data(train_data_dir)
+        break
+    except Exception:
+        print('Ошибка, проверьте корректность пути')
 
-print('Проводится загрузка предложенных данных...')
-start_time = time.time()
-images, labels, labels_str = load_data(train_data_dir)
 print('Загрузка изображений была успешно завершена за ', time.time() - start_time, ' милисекунд')
 print('Всего загружено ', len(images), ' изображений')
 
@@ -113,12 +108,14 @@ print('Сжатие изображений завершено за ', time.time(
 images_a = np.array(images32)
 labels_a = np.array(labels)
 
-print('Создание графа.')
+print('Создание графа...')
 graph = tf.Graph()
 
 print('Введите параметры обучения:')
 learning_rate = float(input("Скорость обучения (learning rate): "))
+# learning_rate = 0.1
 learning_steps = int(input("Количество итераций обучения: "))
+# learning_steps = 100
 
 with graph.as_default():
     images_ph = tf.placeholder(tf.float32, [None, 32, 32, 4])
@@ -149,16 +146,22 @@ while True:
           '2: Распознать изображения; \n'
           '3: Выход.')
     type = int(input("Введите число: "))
-
     if type == 1 :
-        test_data(labels_str)
+        try:
+            test_data(labels_str)
+        except Exception:
+            print('Ошибка, проверьте корректность пути')
     elif type == 2:
         while True:
-            run_on_image(labels_str)
-            plt.close()
-            ans = input("Продолжить распознавания изображений? y/n ")
-            if ans == 'n':
-                break
+            try:
+                run_on_image(labels_str)
+                plt.ion()
+                plt.show()
+            except Exception:
+                print('Ошибка, проверьте корректность пути')
+                ans = input('Продолжить распознавания изображений? y/n: ')
+                if ans == 'n':
+                    break
     elif type == 3:
         break
 
